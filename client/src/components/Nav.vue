35c49ae330ccf650d102e152de21fb09e8add673
Brayden Girard <bgirard@sl.on.ca>
2018-10-05 00:28:10 -0400

    Added nav and hero

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="nav" :class="{ 'nav-background': solid }">
+    <div class="container">
+      <div class="row desktop-only">
+        <div class="column column-40 logo-text"><router-link to="/">Brayden Girard</router-link></div>
+        <div class="column column-30">
+          <router-link to="/projects">Projects</router-link>
+          <router-link to="/podcasts">Podcasts</router-link>
+          <router-link to="/blog">Blog</router-link>
+        </div>
+        <div class="column column-20 contact"><router-link to="/contact">Contact</router-link></div>
+      </div>
+      <div class="row mobile-only">
+        <div class="column logo-text logo-text-mobile"><router-link to="/">Brayden Girard</router-link></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    solid: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #nav {
+    padding: 10px;
+    text-align: left;
+  }
+
+  .nav-background {
+    background-color: var(--black-color);
+  }
+
+  #nav a {
+    font-size: 18px;
+    color: var(--white-color);
+    margin: 0 25px;
+    line-height: 50px;
+    vertical-align: middle;
+  }
+
+  #nav .logo-text a {
+    font-size: 40px;
+    line-height: 50px;
+    font-weight: bold;
+  }
+
+  #nav .contact {
+    text-align: right;
+  }
+
+  .desktop-only {
+    display: flex;
+  }
+  
+  .mobile-only {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    .desktop-only {
+      display: none;
+    }
+    
+    .mobile-only {
+      display: flex;
+    }
+
+    #nav .logo-text-mobile a {
+      font-size: 38px;
+      margin: 0;
+    }
+
+    #nav .logo-text-mobile {
+      text-align: center;
+    }
+  }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <section id="hero">
+      <Nav />
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Nav from '@/components/Nav.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
+
+<style scoped>
+  #hero {
+    height: 805px;
+    background-image: url("../assets/images/hero.jpg");
+    background-size: cover;
+    background-position: center; 
+  }
+
+  @media (max-width: 768px) {
+    #hero { 
+      background-image: url("../assets/images/hero-small.jpg");
+    }
+  }
+</style>
