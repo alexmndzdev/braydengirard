4cf137269f1bf782a7202e8ac7b59226243c48c5
Brayden Girard <bgirard@sl.on.ca>
2018-10-07 15:53:48 -0400

    Added mobile responsive

---
+++ client/src/components/HeroCard.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="card">
+  <img class="inline-block" :src="icon" :alt="title" />
+  <h2 class="inline-block">{{ title }}</h2>
+</div>
+</template>
+<script>
+export default {
+  props: ['icon', 'title']
+}
+</script>
+<style scoped>
+.card {
+  height: 88px;
+  border-radius: 4px;
+  background-color: #FFFFFF;
+  box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
+  margin: 10px 0;
+}
+.card h2 {
+    	color: #9B9B9B;	
+      font-size: 24px;	
+      line-height: 31px;	
+      text-align: center;
+  }
+@media (max-width: 768px) {
+  .card {
+    height: 75px;
+  }
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,17 @@
   <div id="home">
     <section id="hero">
       <Nav />
+        <div class="container">
+          <div class="row cards">
+            <div class="column column-offset-50">
+              <p>You can find me here</p>
+              <HeroCard title="View All Projects" icon="../assets/images/project-bar.svg" />
+              <HeroCard title="View All Projects" icon="../assets/images/podcast-bar.svg" />
+              <HeroCard title="View All Projects" icon="../assets/images/blog-bar.svg" />
+              <HeroCard title="View All Projects" icon="../assets/images/social-bar.svg" />
+            </div>
+          </div>
+        </div>
     </section>
   </div>
 </template>
@@ -9,11 +20,13 @@
 <script>
 // @ is an alias to /src
 import Nav from '@/components/Nav.vue'
+import HeroCard from '@/components/HeroCard.vue'
 
 export default {
   name: 'home',
   components: {
-    Nav
+    Nav,
+    HeroCard
   }
 }
 </script>
@@ -26,9 +39,27 @@ export default {
     background-position: center; 
   }
 
+  .row.cards {
+    margin-top: 18px;
+  }
+
+  .row.cards p {
+    margin-top: 59px;
+    line-height: 38px;
+    text-align: center;
+    color: var(--white-color);
+    font-size: 30px;
+  }
+
   @media (max-width: 768px) {
     #hero { 
       background-image: url("../assets/images/hero-small.jpg");
+    }
+    .row .column.column-offset-50 {
+      margin: 0;
+    }
+    .row.cards p {
+      margin-top: 20px;
     }
   }
 </style>
